68081692a79786f4a5da15cee1591e313d0afa54
Vincent Hou <vincent.houyi@gmail.com>
2020-07-19 17:56:16 +0800

    Minor change on syscall

--- kernel/src/syscall.rs
+++ kernel/src/syscall.rs
@@ -50,22 +50,17 @@ fn _handle_syscall(tcb: &mut TcbObj) -> SysResult<()> {
             let cap_idx = tcb.get_mr(0);
             let cspace = tcb.cspace()?;
             let cap_slot = cspace.lookup_slot(cap_idx)?;
+            let cap = RamCap::try_from(cap_slot)?;
 
-            if let Ok(cap) = RamCap::try_from(cap_slot) {
-                let dst_cptr = tcb.get_mr(1);
+            let dst_cptr = tcb.get_mr(1);
+            let dst_slot = cspace.lookup_slot(dst_cptr)?;
+            let dst_cap = NullCap::try_from(dst_slot)?;
+            let dst_cap = dst_cap.insert::<RamObj>(cap.raw());
 
-                let dst_slot = cspace.lookup_slot(dst_cptr)?;
-                let dst_cap = NullCap::try_from(dst_slot)?;
+            dst_cap.set_mapped_vaddr_asid(0, 0);
 
-                let dst_cap = dst_cap.insert::<RamObj>(cap.raw());
-
-                dst_cap.set_mapped_vaddr_asid(0, 0);
-
-                tcb.set_respinfo(RespInfo::new(SysError::OK, 0));
-                Ok(())
-            } else {
-                Err(SysError::UnsupportedSyscallOp)
-            }
+            tcb.set_respinfo(RespInfo::new(SysError::OK, 0));
+            Ok(())
         },
         SyscallOp::Retype => {
             if msginfo.get_length() < 4 {
@@ -74,23 +69,20 @@ fn _handle_syscall(tcb: &mut TcbObj) -> SysResult<()> {
             let cap_idx = tcb.get_mr(0);
             let cspace = tcb.cspace()?;
             let cap_slot = cspace.lookup_slot(cap_idx)?;
+            let cap = UntypedCap::try_from(cap_slot)?;
 
-            if let Ok(cap) = UntypedCap::try_from(cap_slot) {
-                let obj_type = ObjType::from_usize(tcb.get_mr(1))
-                                        .ok_or(SysError::InvalidValue)?;
-                let bit_size = tcb.get_mr(2);
-                let slot_start = tcb.get_mr(3);
-                let _slot_len = tcb.get_mr(4); //TODO: find some way to do batch op
-                let slots = &cspace[slot_start];
+            let obj_type = ObjType::from_usize(tcb.get_mr(1))
+                                    .ok_or(SysError::InvalidValue)?;
+            let bit_size = tcb.get_mr(2);
+            let slot_start = tcb.get_mr(3);
+            let _slot_len = tcb.get_mr(4); //TODO: find some way to do batch op
+            let slots = &cspace[slot_start];
 
-                cap.retype(obj_type, bit_size, core::slice::from_ref(slots))?;
+            cap.retype(obj_type, bit_size, core::slice::from_ref(slots))?;
 
-                tcb.set_respinfo(RespInfo::new(SysError::OK, 0));
+            tcb.set_respinfo(RespInfo::new(SysError::OK, 0));
 
-                Ok(())
-            } else {
-                Err(SysError::UnsupportedSyscallOp)
-            }
+            Ok(())
         },
         SyscallOp::TcbConfigure => {
             if msginfo.get_length() < 2 {
@@ -99,31 +91,28 @@ fn _handle_syscall(tcb: &mut TcbObj) -> SysResult<()> {
             let cap_idx = tcb.get_mr(0);
             let host_cspace = tcb.cspace()?;
             let cap_slot = host_cspace.lookup_slot(cap_idx)?;
+            let cap = TcbCap::try_from(cap_slot)?;
 
-            TcbCap::try_from(cap_slot)
-                .map_err(|_| SysError::UnsupportedSyscallOp)
-                .and_then(|cap| {
-                    let vspace_cap_idx = tcb.get_mr(1);
-                    let vspace_cap = if vspace_cap_idx != 0 {
-                        let vspace_slot = host_cspace.lookup_slot(vspace_cap_idx)?;
-                        Some(VTableCap::try_from(vspace_slot)?)
-                    } else {
-                        None
-                    };
+            let vspace_cap_idx = tcb.get_mr(1);
+            let vspace_cap = if vspace_cap_idx != 0 {
+                let vspace_slot = host_cspace.lookup_slot(vspace_cap_idx)?;
+                Some(VTableCap::try_from(vspace_slot)?)
+            } else {
+                None
+            };
 
-                    let cspace_cap_idx = tcb.get_mr(2);
-                    let cspace_cap = if cspace_cap_idx != 0 {
-                        let cspace_slot = host_cspace.lookup_slot(cspace_cap_idx)?;
-                        Some(CNodeCap::try_from(cspace_slot)?)
-                    } else {
-                        None
-                    };
+            let cspace_cap_idx = tcb.get_mr(2);
+            let cspace_cap = if cspace_cap_idx != 0 {
+                let cspace_slot = host_cspace.lookup_slot(cspace_cap_idx)?;
+                Some(CNodeCap::try_from(cspace_slot)?)
+            } else {
+                None
+            };
 
-                    cap.configure(cspace_cap, vspace_cap)?;
+            cap.configure(cspace_cap, vspace_cap)?;
 
-                    tcb.set_respinfo(RespInfo::new(SysError::OK, 0));
-                    Ok(())
-                })
+            tcb.set_respinfo(RespInfo::new(SysError::OK, 0));
+            Ok(())
         },
         SyscallOp::TcbSetRegisters => {
             if msginfo.get_length() < 3 {
@@ -133,45 +122,42 @@ fn _handle_syscall(tcb: &mut TcbObj) -> SysResult<()> {
             let cap_idx = tcb.get_mr(0);
             let cspace = tcb.cspace()?;
             let cap_slot = cspace.lookup_slot(cap_idx)?;
-            TcbCap::try_from(cap_slot)
-                .map_err(|_| SysError::UnsupportedSyscallOp)
-                .and_then(|cap| {
-                    let reg_flags = tcb.get_mr(1);
-                    if reg_flags & 0b1000 == 0b1000 {
-                        let elr = tcb.get_mr(2);
-                        cap.tf.set_elr(elr);
-                    }
+            let cap = TcbCap::try_from(cap_slot)?;
 
-                    if reg_flags & 0b0100 == 0b0100 {
-                        let sp = tcb.get_mr(3);
-                        cap.tf.set_sp(sp);
-                    }
+            let reg_flags = tcb.get_mr(1);
+            if reg_flags & 0b1000 == 0b1000 {
+                let elr = tcb.get_mr(2);
+                cap.tf.set_elr(elr);
+            }
 
-                    tcb.set_respinfo(RespInfo::new(SysError::OK, 0));
-                    Ok(())
-                })
+            if reg_flags & 0b0100 == 0b0100 {
+                let sp = tcb.get_mr(3);
+                cap.tf.set_sp(sp);
+            }
+
+            tcb.set_respinfo(RespInfo::new(SysError::OK, 0));
+            Ok(())
         },
         SyscallOp::TcbResume => {
             let cap_idx = tcb.get_mr(0);
             let cspace = tcb.cspace()?;
             let cap_slot = cspace.lookup_slot(cap_idx)?;
 
-            TcbCap::try_from(cap_slot)
-                .map_err(|_| SysError::UnsupportedSyscallOp)
-                .and_then(|mut cap| {
-                    crate::SCHEDULER.push(&cap);
-                    tcb.set_respinfo(RespInfo::new(SysError::OK, 0));
-                    Ok(())
-                })
+            let cap = TcbCap::try_from(cap_slot)?;
+            crate::SCHEDULER.push(&cap);
+
+            tcb.set_respinfo(RespInfo::new(SysError::OK, 0));
+            Ok(())
         },
         SyscallOp::EndpointSend => {
             let cap_idx = tcb.get_mr(0);
             let cspace = tcb.cspace()?;
             let cap_slot = cspace.lookup_slot(cap_idx)?;
 
-            EndpointCap::try_from(cap_slot)
-                .map_err(|_| SysError::UnsupportedSyscallOp)
-                .and_then(|cap| cap.handle_send(msginfo, tcb))
+            let cap = EndpointCap::try_from(cap_slot)?;
+            cap.handle_send(msginfo, tcb)?;
+
+            Ok(())
         }
         SyscallOp::EndpointRecv => {
             let cap_idx = tcb.get_mr(0);
@@ -180,7 +166,9 @@ fn _handle_syscall(tcb: &mut TcbObj) -> SysResult<()> {
 
             EndpointCap::try_from(cap_slot)
                 .map_err(|_| SysError::UnsupportedSyscallOp)
-                .and_then(|cap| cap.handle_recv(msginfo, tcb))
+                .and_then(|cap| cap.handle_recv(msginfo, tcb));
+
+            Ok(())
         }
         SyscallOp::EndpointCall => {
             unimplemented!()
@@ -199,25 +187,23 @@ fn _handle_syscall(tcb: &mut TcbObj) -> SysResult<()> {
             let cspace = tcb.cspace()?;
             let cap_slot = cspace.lookup_slot(cap_idx)?;
 
-            if let Ok(cap) = RamCap::try_from(cap_slot) {
-                if cap.mapped_vaddr() != 0 {
-                    return Err(SysError::VSpaceError)
-                }
+            let cap = RamCap::try_from(cap_slot)?;
 
-                let vspace_cap_idx = tcb.get_mr(1);
-                let vaddr = tcb.get_mr(2);
-                let rights = tcb.get_mr(3).into();
-
-                let vspace_cap_slot = cspace.lookup_slot(vspace_cap_idx)?;
-                let vspace = VSpace::from_pgd(&*(VTableCap::try_from(vspace_cap_slot)?));
-
-                cap.map_page(&vspace, vaddr, rights)?;
-
-                tcb.set_respinfo(RespInfo::new(SysError::OK, 0));
-                Ok(())
-            } else {
-                Err(SysError::UnsupportedSyscallOp)
+            if cap.mapped_vaddr() != 0 {
+                return Err(SysError::VSpaceError)
             }
+
+            let vspace_cap_idx = tcb.get_mr(1);
+            let vaddr = tcb.get_mr(2);
+            let rights = tcb.get_mr(3).into();
+
+            let vspace_cap_slot = cspace.lookup_slot(vspace_cap_idx)?;
+            let vspace = VSpace::from_pgd(&*(VTableCap::try_from(vspace_cap_slot)?));
+
+            cap.map_page(&vspace, vaddr, rights)?;
+
+            tcb.set_respinfo(RespInfo::new(SysError::OK, 0));
+            Ok(())
         },
         SyscallOp::VTableMap => {
             use crate::vspace::VSpace;
@@ -230,25 +216,23 @@ fn _handle_syscall(tcb: &mut TcbObj) -> SysResult<()> {
             let cspace = tcb.cspace()?;
             let cap_slot = cspace.lookup_slot(cap_idx)?;
 
-            if let Ok(cap) = VTableCap::try_from(cap_slot) {
-                if cap.mapped_vaddr() != 0 {
-                    return Err(SysError::VSpaceError)
-                }
+            let cap = VTableCap::try_from(cap_slot)?;
 
-                let vspace_cap_idx = tcb.get_mr(1);
-                let vaddr = tcb.get_mr(2);
-                let level = tcb.get_mr(3);
-
-                let vspace_cap_slot = cspace.lookup_slot(vspace_cap_idx)?;
-                let vspace = VSpace::from_pgd(&*(VTableCap::try_from(vspace_cap_slot)?));
-
-                cap.map_vtable(&vspace, vaddr, level)?;
-
-                tcb.set_respinfo(RespInfo::new(SysError::OK, 0));
-                Ok(())
-            } else {
-                Err(SysError::UnsupportedSyscallOp)
+            if cap.mapped_vaddr() != 0 {
+                return Err(SysError::VSpaceError)
             }
+
+            let vspace_cap_idx = tcb.get_mr(1);
+            let vaddr = tcb.get_mr(2);
+            let level = tcb.get_mr(3);
+
+            let vspace_cap_slot = cspace.lookup_slot(vspace_cap_idx)?;
+            let vspace = VSpace::from_pgd(&*(VTableCap::try_from(vspace_cap_slot)?));
+
+            cap.map_vtable(&vspace, vaddr, level)?;
+
+            tcb.set_respinfo(RespInfo::new(SysError::OK, 0));
+            Ok(())
         },
         SyscallOp::MonitorMintUntyped => {
             if msginfo.get_length() < 4 {
@@ -258,23 +242,21 @@ fn _handle_syscall(tcb: &mut TcbObj) -> SysResult<()> {
             let cspace = tcb.cspace()?;
             let cap_slot = cspace.lookup_slot(cap_idx)?;
 
-            if let Ok(_) = MonitorCap::try_from(cap_slot) {
-                let slot = tcb.get_mr(1);
-                let paddr = tcb.get_mr(2);
-                let bit_size = tcb.get_mr(3);
-                let is_device = tcb.get_mr(4) == 1;
+            let cap = MonitorCap::try_from(cap_slot)?;
 
-                let cap = cspace.lookup_slot(slot)?;
+            let slot = tcb.get_mr(1);
+            let paddr = tcb.get_mr(2);
+            let bit_size = tcb.get_mr(3);
+            let is_device = tcb.get_mr(4) == 1;
 
-                NullCap::try_from(cap)?
-                    .insert::<UntypedObj>(UntypedCap::mint(paddr, bit_size, is_device));
+            let cap = cspace.lookup_slot(slot)?;
 
-                tcb.set_respinfo(RespInfo::new(SysError::OK, 0));
+            NullCap::try_from(cap)?
+                .insert::<UntypedObj>(UntypedCap::mint(paddr, bit_size, is_device));
 
-                Ok(())
-            } else {
-                Err(SysError::UnsupportedSyscallOp)
-            }
+            tcb.set_respinfo(RespInfo::new(SysError::OK, 0));
+
+            Ok(())
         },
         SyscallOp::MonitorInsertTcbToCpu => {
             use crate::scheduler::SCHEDULER;
@@ -285,59 +267,55 @@ fn _handle_syscall(tcb: &mut TcbObj) -> SysResult<()> {
             let cspace = tcb.cspace()?;
             let cap_slot = cspace.lookup_slot(cap_idx)?;
 
-            if let Ok(_) = MonitorCap::try_from(cap_slot) {
-                let tcb_idx = tcb.get_mr(1);
-                let cpu = tcb.get_mr(2);
+            MonitorCap::try_from(cap_slot)?;
 
-                let tcb_slot = cspace.lookup_slot(tcb_idx)?;
+            let tcb_idx = tcb.get_mr(1);
+            let cpu = tcb.get_mr(2);
 
-                let tcb_cap = TcbCap::try_from(tcb_slot)?;
-                unsafe {
-                    SCHEDULER.get_unsafe(cpu)
-                        .push(&tcb_cap);
-                }
+            let tcb_slot = cspace.lookup_slot(tcb_idx)?;
 
-                tcb.set_respinfo(RespInfo::new(SysError::OK, 0));
-
-                Ok(())
-            } else {
-                Err(SysError::UnsupportedSyscallOp)
+            let tcb_cap = TcbCap::try_from(tcb_slot)?;
+            unsafe {
+                SCHEDULER.get_unsafe(cpu)
+                    .push(&tcb_cap);
             }
+
+            tcb.set_respinfo(RespInfo::new(SysError::OK, 0));
+
+            Ok(())
         },
         SyscallOp::InterruptAttachIrq => {
+            use core::cell::Cell;
+
             let cap_idx = tcb.get_mr(0);
             let cspace = tcb.cspace()?;
             let cap_slot = cspace.lookup_slot(cap_idx)?;
 
-            if let Ok(_cap) = InterruptCap::try_from(cap_slot) {
-                use core::cell::Cell;
+            InterruptCap::try_from(cap_slot)?;
 
-                let ep = tcb.get_mr(1);
-                let irq = tcb.get_mr(2);
+            let ep = tcb.get_mr(1);
+            let irq = tcb.get_mr(2);
 
-                let ep_slot = cspace.lookup_slot(ep)?;
-                let ep_cap = EndpointCap::try_from(ep_slot)?;
+            let ep_slot = cspace.lookup_slot(ep)?;
+            let ep_cap = EndpointCap::try_from(ep_slot)?;
 
-                ep_cap.set_attach(crate::objects::Attach::Irq(irq));
+            ep_cap.set_attach(crate::objects::Attach::Irq(irq));
 
-                unsafe {
-                    crate::interrupt::INTERRUPT_CONTROLLER.lock().attach_irq(irq, Cell::new(ep_cap.raw()));
-                }
-
-                tcb.set_respinfo(RespInfo::new(SysError::OK, 0));
-
-                Ok(())
-            } else {
-                Err(SysError::UnsupportedSyscallOp)
+            unsafe {
+                crate::interrupt::INTERRUPT_CONTROLLER.lock().attach_irq(irq, Cell::new(ep_cap.raw()));
             }
+
+            tcb.set_respinfo(RespInfo::new(SysError::OK, 0));
+
+            Ok(())
         }
     }
 }
 
 pub fn handle_syscall(tcb: &mut TcbObj) -> ! {
-    let ret = _handle_syscall(tcb);
 
-    if let Err(e) = ret {
+    if let Err(e) = _handle_syscall(tcb) {
+        kprintln!("Syscall Error {:?} TCB {:?}", e, tcb);
         tcb.set_respinfo(RespInfo::new(e, 0));
     }
 
