3a2620f000376fdafc9e861904c6673bc87baf69
Vincent Hou <vincent.houyi@gmail.com>
2020-08-21 21:32:05 +0800

    impl async read/write on console

--- init_thread/src/console.rs
+++ init_thread/src/console.rs
@@ -1,135 +1,127 @@
-use spin::{Mutex, MutexGuard};
-use pi::uart::{MiniUart, IrqStatus};
-use conquer_once::spin::OnceCell;
 use core::future::Future;
 use core::{pin::Pin, task::{Poll, Context}};
+use core::task::Waker;
+
+use alloc::collections::LinkedList;
+use alloc::sync::Arc;
+
 use futures_util::stream::Stream;
-use crossbeam_queue::SegQueue;
+use spin::Mutex;
 
-/*
+use pi::uart::{MiniUart, IrqStatus};
+use naive::ep_server::{EpServer, EpNtfHandler};
+
 pub struct Console {
-    inner: Option<MiniUart>,
-    tx_buf: LinkedList<u8>,
-    rx_buf: LinkedList<u8>,
+    inner: MiniUart,
+    rx_waker: LinkedList<Waker>,
+    tx_waker: LinkedList<Waker>,
 }
-
 impl Console {
-    pub const fn new() -> Console {
-        Console { inner: None, tx_buf: LinkedList::new(), rx_buf: LinkedList::new() }
+    pub const fn new(mini_uart: MiniUart) -> Console {
+        Console { inner: mini_uart, tx_waker: LinkedList::new(), rx_waker: LinkedList::new() }
     }
 
-    pub fn initialize(&mut self, mini_uart: MiniUart) {
-        if let None = self.inner {
-            self.inner = Some(mini_uart);
-        }
+    pub fn can_read(&self) -> bool {
+        self.inner.has_byte()
     }
 
-    pub fn try_read_byte(&mut self) -> Option<u8> {
-        self.rx_buf.pop_front()
+    pub fn can_write(&self) -> bool {
+        self.inner.can_write()
+    }
+
+    pub fn read_byte(&mut self) -> u8 {
+        self.inner.read_byte()
     }
 
     pub fn write_byte(&mut self, byte: u8) {
-        let was_empty = self.tx_buf.is_empty();
-        self.tx_buf.push_back(byte);
-
-        if let Some(inner) = self.inner.as_mut() {
-            if was_empty {
-                inner.enable_tx_irq();
-            }
-        }
-    }
-
-    pub fn read_from_inner(&mut self) {
-        if let Some(inner) = self.inner.as_mut() {
-            if inner.has_byte() {
-                let c = inner.read_byte();
-                self.rx_buf.push_back(c);
-            }
-        }
-    }
-
-    pub fn write_to_inner(&mut self) {
-        if let Some(inner) = self.inner.as_mut() {
-            // TODO: figure out why it lose some irq
-//            if let Some(c) = self.tx_buf.pop_front() {
-//                inner.write_byte(c);
-//            } else {
-//                inner.disable_tx_irq();
-//            }
-            while let Some(c) = self.tx_buf.pop_front() {
-                inner.write_byte(c);
-            }
-            inner.disable_tx_irq();
-        }
+        self.inner.write_byte(byte)
     }
 
     pub fn enable_tx_irq(&mut self) {
-        if let Some(inner) = self.inner.as_mut() {
-            inner.enable_tx_irq();
-        }
+        self.inner.enable_tx_irq();
     }
 
     pub fn enable_rx_irq(&mut self) {
-        if let Some(inner) = self.inner.as_mut() {
-            inner.enable_rx_irq();
-        }
+        self.inner.enable_rx_irq();
+    }
+
+    pub fn disable_tx_irq(&mut self) {
+        self.inner.disable_tx_irq();
+    }
+
+    pub fn disable_rx_irq(&mut self) {
+        self.inner.disable_rx_irq();
     }
 
     pub fn irq_status(&mut self) -> IrqStatus {
-        if let Some(inner) = self.inner.as_mut() {
-            inner.irq_status()
-        } else {
-            IrqStatus::Clear
+        self.inner.irq_status()
+    }
+}
+
+#[derive(Clone)]
+pub struct ConsoleExt {
+    inner: Arc<Mutex<Console>>
+}
+
+impl ConsoleExt {
+    pub fn new(console: Console) -> Self {
+        Self { inner: Arc::new(Mutex::new(console)) }
+    }
+
+    pub fn stream(&self) -> ConsoleReader {
+        ConsoleReader { inner: self.clone() }
+    }
+
+    pub fn can_read(&self) -> bool { self.inner.lock().can_read() }
+
+    pub fn read_byte(&self) -> u8 { self.inner.lock().read_byte() }
+
+    pub fn can_write(&self) -> bool { self.inner.lock().can_write() }
+
+    pub fn write_byte(&self, byte: u8) { self.inner.lock().write_byte(byte) }
+
+    pub fn poll_write<'a>(&self, buf: &'a [u8]) -> WriteFuture<'a> {
+        WriteFuture {
+            inner : self.clone(),
+            buf: buf,
+            write_len: 0,
         }
     }
 }
 
-impl core::fmt::Write for Console {
-    fn write_str(&mut self, s: &str) -> core::fmt::Result {
-        for byte in s.bytes() {
-            if byte == b'\n' {
-                self.write_byte(b'\r');
+impl EpNtfHandler for ConsoleExt {
+    fn handle_notification(&self, ep_server: &EpServer, ntf: usize) {
+        let mut inner = self.inner.lock();
+        match inner.irq_status() {
+            IrqStatus::Rx => {
+                // TODO: find out why irq is generated when tx/rx irq disabled
+                // if inner.tx_waker.is_empty() {
+                //     kprintln!("rx_waker is empty");
+                // }
+                while let Some(waker) = inner.rx_waker.pop_front() {
+                    waker.wake();
+                }
+                inner.disable_rx_irq();
             }
-            self.write_byte(byte);
-        }
-        Ok(())
-    }
-}
-
-pub static CONSOLE: Mutex<Console> = Mutex::new(Console::new());
-
-pub fn console_main() -> ! {
-    use rustyl4api::object::{Capability, EndpointObj, InterruptObj};
-    use naive::space_manager::gsm;
-
-    let irq_ep = gsm!().alloc_object::<EndpointObj>(12).unwrap();
-    let irq_cntl_cap = Capability::<InterruptObj>::new(rustyl4api::init::InitCSpaceSlot::IrqController as usize);
-    irq_cntl_cap.attach_ep_to_irq(irq_ep.slot, pi::interrupt::Interrupt::Aux as usize).unwrap();
-
-    {
-        let mut con = CONSOLE.lock();
-        if con.tx_buf.is_empty() {
-            con.enable_tx_irq();
-        }
-
-        con.enable_rx_irq();
-    }
-
-    loop {
-        irq_ep.receive(None).unwrap();
-        let mut con = CONSOLE.lock();
-
-        loop {
-            match con.irq_status() {
-                IrqStatus::Tx => { con.write_to_inner() }
-                IrqStatus::Rx => { con.read_from_inner() }
-                IrqStatus::Clear => { break }
+            IrqStatus::Tx => {
+                // if inner.tx_waker.is_empty() {
+                //     kprintln!("tx_waker is empty");
+                // }
+                while let Some(waker) = inner.tx_waker.pop_front() {
+                    waker.wake();
+                }
+                inner.disable_tx_irq();
+            }
+            IrqStatus::Clear => {
+                kprintln!("in clear");
             }
         }
     }
 }
 
-pub fn init_console_server() {
+static CONSOLE: Mutex<Option<ConsoleExt>> = Mutex::new(None);
+
+pub fn console_server() -> ConsoleExt {
     use crate::gpio;
     use pi::gpio::Function;
 
@@ -140,117 +132,65 @@ pub fn init_console_server() {
     let mut uart = MiniUart::new(uart_base.as_ptr() as usize);
     uart.initialize(115200);
 
-    CONSOLE.lock().initialize(uart);
+    let mut con = Console::new(uart);
+    con.disable_rx_irq();
+    con.disable_tx_irq();
+    let con = ConsoleExt::new(con);
 
-    naive::thread::spawn(console_main);
+    *CONSOLE.lock() = Some(con.clone());
+
+    con
 }
 
-pub fn console_read_byte() -> u8 {
+pub fn console() -> ConsoleExt {
     loop {
-        if let Some(b) = CONSOLE.lock().try_read_byte() {
-            return b;
+        if let Some(c) = &*CONSOLE.lock() {
+            return c.clone();
         }
     }
 }
 
-pub fn console_print(args: core::fmt::Arguments) {
-    use core::fmt::Write;
-    CONSOLE.lock().write_fmt(args).unwrap();
-}
-
-pub macro println {
-    () => (print!("\n")),
-    ($fmt:expr) => (print!(concat!($fmt, "\n"))),
-    ($fmt:expr, $($arg:tt)*) => (print!(concat!($fmt, "\n"), $($arg)*))
-}
-
-pub macro print($($arg:tt)*) {
-    console_print(format_args!($($arg)*))
-}
-*/
-
-pub fn console() -> MutexGuard<'static, MiniUart> {
-    static CONSOLE: OnceCell<Mutex<MiniUart>> = OnceCell::uninit();
-
-    CONSOLE.try_get_or_init(|| {
-        use crate::gpio;
-        use pi::gpio::Function;
-
-        kprintln!("init console");
-        gpio::GPIO_SERVER.lock().as_mut().unwrap().get_pin(14).unwrap().into_alt(Function::Alt5);
-        gpio::GPIO_SERVER.lock().as_mut().unwrap().get_pin(15).unwrap().into_alt(Function::Alt5);
-
-        let uart_base = naive::space_manager::allocate_frame_at(0x3f215000, 4096).unwrap();
-        let mut uart = MiniUart::new(uart_base.as_ptr() as usize);
-        uart.initialize(115200);
-
-        // CONSOLE.lock().initialize(uart);
-
-        Mutex::new(uart)
-    }).unwrap().lock()
-}
-
-pub fn tx_buf() -> &'static SegQueue<u8> {
-    static TX_BUF: OnceCell<SegQueue<u8>> = OnceCell::uninit();
-
-    TX_BUF.try_get_or_init(|| SegQueue::new()).unwrap()
-}
-
-pub fn rx_buf() -> &'static SegQueue<u8> {
-    static RX_BUF: OnceCell<SegQueue<u8>> = OnceCell::uninit();
-
-    RX_BUF.try_get_or_init(|| SegQueue::new()).unwrap()
-}
-
-struct ConsoleReader { }
-
-impl ConsoleReader {
-    pub fn new() -> Self {
-        Self { }
-    }
+pub struct ConsoleReader {
+    inner: ConsoleExt
 }
 
 impl Stream for ConsoleReader {
     type Item = u8;
 
     fn poll_next(self: Pin<&mut Self>, cx: &mut Context) -> Poll<Option<u8>> {
-        // let mut con = CONSOLE.lock();
-        let mut con = console();
-        if con.has_byte() {
-            Poll::Ready(Some(con.read_byte()))
+        if self.inner.can_read() {
+            Poll::Ready(Some(self.inner.read_byte()))
         } else {
+            let mut inner = self.inner.inner.lock();
+            inner.rx_waker.push_back(cx.waker().clone());
+            inner.enable_rx_irq();
             Poll::Pending
         }
     }
 }
 
-pub async fn read_from_uart() {
-    use futures_util::StreamExt;
-
-    while let Some(byte) = ConsoleReader::new().next().await {
-        rx_buf().push(byte)
-    }
+pub struct WriteFuture<'a> {
+    inner: ConsoleExt,
+    buf: &'a [u8],
+    write_len: usize,
 }
 
-struct ConsoleWriter { }
-
-impl Future for ConsoleWriter {
-    type Output = ();
+impl<'a> Future for WriteFuture<'a> {
+    type Output = usize;
     fn poll(mut self: Pin<&mut Self>, cx: &mut Context) -> Poll<Self::Output> {
-        let mut con = console();
-        while con.can_write() {
-            if let Some(byte) = tx_buf().pop().ok() {
-                con.write_byte(byte)
+        while self.write_len < self.buf.len() {
+            if !self.inner.can_write() {
+                let mut inner = self.inner.inner.lock();
+                inner.tx_waker.push_back(cx.waker().clone());
+                inner.enable_tx_irq();
+                return Poll::Pending;
             } else {
-                break;
+                let byte = self.buf[self.write_len];
+                self.inner.write_byte(byte);
+                self.write_len += 1;
             }
         }
-        Poll::Pending
-    }
-}
 
-pub async fn write_to_uart() {
-    loop {
-        let writer = ConsoleWriter { }.await;
+        Poll::Ready(self.write_len)
     }
 }
