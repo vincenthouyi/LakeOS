2013d61db4a559801823219abf470afd416f5972
Vincent Hou <vincent.houyi@gmail.com>
2020-12-10 19:54:14 +0800

    make async thread's stack to 16k

--- naive/src/thread.rs
+++ naive/src/thread.rs
@@ -6,16 +6,14 @@ use crate::space_manager::gsm;
 pub fn spawn(entry: fn() -> !) {
     use rustyl4api::vspace::{FRAME_SIZE, Permission};
 
+    let npages = 4;
     let tcb = gsm!().alloc_object::<TcbObj>(12)
                         .unwrap();
 
-    let stack_ram = gsm!().alloc_object::<RamObj>(12)
-                              .unwrap();
-
-    let stack_base = gsm!().insert_ram_at(stack_ram, 0, Permission::writable()) as usize;
+    let stack_base = gsm!().map_frame_at(0, 0, FRAME_SIZE * npages, Permission::writable()).unwrap() as usize;
     tcb.configure(Some(RootVNodeCap as usize), Some(RootCNodeCap as usize))
        .expect("Error Configuring TCB");
-    tcb.set_registers(0b1100,entry as usize, stack_base + FRAME_SIZE)
+    tcb.set_registers(0b1100,entry as usize, stack_base + FRAME_SIZE * npages)
        .expect("Error Setting Registers");
     tcb.resume()
        .expect("Error Resuming TCB");
